d2f264eba78a2c261d2afb379dfc94f55ff9ea99
Edward Lee <7edwardlee@gmail.com>
2024-03-31 16:02:13 -0700

    added navbar

--- campus-connect/src/App.js
+++ campus-connect/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './Landing';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          {/* <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
