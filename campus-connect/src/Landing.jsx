55acc8f4d2af50bf5dc1e3986fadd4efe1d4a3a2
Edward Lee <7edwardlee@gmail.com>
2024-04-18 18:28:46 -0700

    text

--- campus-connect/src/Landing.jsx
+++ campus-connect/src/Landing.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <div className="">
+        <h1 id="main-logo">CAMPUS CONNECT</h1>
+        <h2 id="sub-logo">Explore new communities and see what UW has to offer</h2>
+      </div>
+    </div>
   )
 }
 
